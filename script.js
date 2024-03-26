7ea3c55df0fd50f8a4ce8a5bde6265c1e7bfa32c
CamPham <VanCam1011>
2024-03-26 18:40:10 +0700

    upload code

---
+++ script.js
@@ -0,0 +1,7 @@
+
+let inner = document.querySelector('.inner');
+inner.addEventListener('mousemove', function(event){
+    document.documentElement.style.setProperty("--left-focus", event.x + 'px');
+    document.documentElement.style.setProperty("--top-focus", event.y + "px");
+})
+ 
